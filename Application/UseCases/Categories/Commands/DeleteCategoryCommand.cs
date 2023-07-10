00720e749c0c092e3aff7fed318ec5259b2a94df
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-07-10 17:02:28 +0300

    aaa

---
+++ Application/UseCases/Categories/Commands/DeleteCategoryCommand.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.UseCases.Categories.Commands;
+public record DeleteCategoryCommand : IRequest
+{
+    public Guid CategoryId { get; set; }
+}
+
+public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteCategoryCommandHandler(IApplicationDbContext context)
+        => _context = context;
+
+    public async Task Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var findCategory = await _context.Category.FindAsync(new object[] { request.CategoryId }, cancellationToken);
+        if (findCategory is null)
+            throw new NotFoundException
+    }
+}
