bf4bdf46a0d29048ccb63a04fb78595b4fd6a642
Elyorbek Tojidinov <tojidinovbekelyorbek@gmail.com>
2023-07-10 18:05:57 +0500

    added handler bookkeeping

--- Application/UseCases/Bookkepings/Queries/GetAllBookkeepingQueries.cs
+++ Application/UseCases/Bookkepings/Queries/GetAllBookkeepingQueries.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,26 @@ using System.Threading.Tasks;
 
 namespace Application.UseCases.Bookkepings.Queries
 {
-    public class GetAllBookkeepingQueries
+    public class GetAllBookkeepingQueries : IRequest<IEnumerable<BookkeepingGetDto>>
     {
 
+    }
+
+    public class GetAllBookkeepingQueriesHandler : IRequestHandler<GetAllBookkeepingQueries, IEnumerable<BookkeepingGetDto>>
+    {
+
+        private readonly IApplicationDbContext _context;
+        private readonly ICurrentUserService _currentUserService;
+
+        public GetAllBookkeepingQueriesHandler(ICurrentUserService currentUserService, IApplicationDbContext context)
+        {
+            _currentUserService = currentUserService;
+            _context = context;
+        }
+
+        public Task<IEnumerable<BookkeepingGetDto>> Handle(GetAllBookkeepingQueries request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
