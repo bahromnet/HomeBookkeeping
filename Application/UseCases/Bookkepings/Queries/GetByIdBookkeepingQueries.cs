84b8e255d418abcdc96494a477a37d2dc0bb41c7
Elyorbek Tojidinov <tojidinovbekelyorbek@gmail.com>
2023-07-13 15:43:48 +0500

    commit

--- Application/UseCases/Bookkepings/Queries/GetByIdBookkeepingQueries.cs
+++ Application/UseCases/Bookkepings/Queries/GetByIdBookkeepingQueries.cs
@@ -1,12 +1,34 @@
-﻿using Application.Common.Models;
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using AutoMapper;
 using MediatR;
-
 
 namespace Application.UseCases.Bookkepings.Queries
 {
     public class GetByIdBookkeepingQueries : IRequest<BookkeepingGetDto>
     {
-        public Guid 
+        public Guid BookkeepingId { get; set; }
+    }
+
+    public class GetByIdBookkeepingQueriesHandler : IRequestHandler<GetByIdBookkeepingQueries, BookkeepingGetDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public GetByIdBookkeepingQueriesHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<BookkeepingGetDto> Handle(GetByIdBookkeepingQueries request, CancellationToken cancellationToken)
+        {
+            var findBookkepping =await _context.Bookkeepings.FindAsync(new object[] {request.BookkeepingId});
+            if (findBookkepping is null)
+                throw new NotFoundException();
+            var bookkeepMap = _mapper.Map<BookkeepingGetDto>(findBookkepping);
+            return bookkeepMap;
+        }
     }
 
 
