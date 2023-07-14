fab8300bf983e88f8d388116aa1ab215d39f4205
Elyorbek Tojidinov <tojidinovbekelyorbek@gmail.com>
2023-07-14 18:19:25 +0500

    added filters

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -8,7 +8,7 @@ public class Category
     /// if ExpenceIncomeType = false type: Expence
     /// else type:Income
     /// </summary>
-    public bool ExpenceIncomeType { get; set; }
+    public string ExpenceIncomeType { get; set; }
 
     public virtual IList<Bookkeeping>? Bookkeepings { get; set; } = new List<Bookkeeping>();
 
